e31e67dea42379b9040d6aa22d4f41ae6986d1c3
Jack Carter <131922119+jackandcarter@users.noreply.github.com>
2023-09-06 02:24:42 -0500

    Delete SettingsPanelController.cs

--- SettingsPanelController.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class SettingsPanelController : MonoBehaviour
-{
-    // Implement methods to adjust player input settings
-    // Save and load settings using PlayerPrefs or your preferred data storage method
-}
